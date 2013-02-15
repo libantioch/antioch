77b62018f616c99a87df3dd410b42770a9fdca52
Paul T. Bauman <ptbauman@gmail.com>
2013-02-15 23:36:40 +0000

    [antioch]: Adding utilities directory as well as source for versioning info (#2756).

---
+++ src/utilities/src/antioch_version.C
@@ -0,0 +1,66 @@
+//-----------------------------------------------------------------------bl-
+//-----------------------------------------------------------------------el-
+//
+// $Id$
+//
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+
+#include "antioch/antioch_version.h"
+
+namespace ANTIOCH
+{
+
+  void antioch_version_stdout()
+  {
+    std::cout << "--------------------------------------------------------" << std::endl;
+    std::cout << "antioch Package: Version = " << ANTIOCH_LIB_VERSION;
+    std::cout << " (" << get_antioch_version() << ")" << std::endl << std::endl;
+  
+    std::cout << ANTIOCH_LIB_RELEASE << std::endl << std::endl;
+  
+    std::cout << "Build Date   = " << ANTIOCH_BUILD_DATE     << std::endl;
+    std::cout << "Build Host   = " << ANTIOCH_BUILD_HOST     << std::endl;
+    std::cout << "Build User   = " << ANTIOCH_BUILD_USER     << std::endl;
+    std::cout << "Build Arch   = " << ANTIOCH_BUILD_ARCH     << std::endl;
+    std::cout << "Build Rev    = " << ANTIOCH_BUILD_VERSION  << std::endl << std::endl;
+  
+    std::cout << "C++ Config   = " << ANTIOCH_CXX << " " << ANTIOCH_CXXFLAGS << std::endl;
+    std::cout << "--------------------------------------------------------" << std::endl;
+  
+    return;
+  }
+
+  int get_antioch_version()
+  {
+    /* Note: return format follows the versioning convention xx.yy.zz where
+   
+       xx = major version number
+       yy = minor version number
+       zz = micro version number
+     
+       For example:
+       v.   0.23  -> 002300 = 2300
+       v   0.23.1 -> 002301 = 2301
+       v. 10.23.2 -> 102302         */
+
+    int major_version = 0;
+    int minor_version = 0;
+    int micro_version = 0;
+
+#ifdef ANTIOCH_MAJOR_VERSION
+    major_version = ANTIOCH_MAJOR_VERSION;
+#endif
+
+#ifdef ANTIOCH_MINOR_VERSION
+    minor_version = ANTIOCH_MINOR_VERSION;
+#endif
+
+#ifdef ANTIOCH_MICRO_VERSION
+    micro_version = ANTIOCH_MICRO_VERSION;
+#endif
+      
+    return major_version*10000 + minor_version*100 + micro_version;
+  }
+
+} // end namespace ANTIOCH
