1b2c650b75d71cfb3bd4e4bb3c78ccc90bbbf8db
Todd Oliver <oliver@ices.utexas.edu>
2013-03-21 17:16:18 +0000

    antioch: (#2804) A failing test, as promised.

--- test/kinetics_unit.C
+++ test/kinetics_unit.C
@@ -43,6 +43,80 @@
 #include "antioch/kinetics_evaluator.h"
 
 template <typename Scalar>
+int tester_N2N(const std::string& input_name)
+{
+  std::vector<std::string> species_str_list;
+  const unsigned int n_species = 2;
+  species_str_list.reserve(n_species);
+  species_str_list.push_back( "N2" );
+  species_str_list.push_back( "N" );
+
+  Antioch::ChemicalMixture<Scalar> chem_mixture( species_str_list );
+  Antioch::ReactionSet<Scalar> reaction_set( chem_mixture );
+  Antioch::CEAThermodynamics<Scalar> thermo( chem_mixture );
+
+  Antioch::read_reaction_set_data_xml<Scalar>( input_name, true, reaction_set );
+
+  Antioch::KineticsEvaluator<Scalar> kinetics( reaction_set );
+  std::vector<Scalar> omega_dot(n_species);
+
+  const Scalar P = 1.0e5;
+
+  // Mass fractions
+  std::vector<Scalar> Y(n_species,0.2);
+
+  const Scalar R_mix = chem_mixture.R(Y);
+
+  const unsigned int n_T_samples = 10;
+  const Scalar T0 = 500;
+  const Scalar T_inc = 500;
+
+  std::vector<Scalar> molar_densities(n_species,0.0);
+  std::vector<Scalar> h_RT_minus_s_R(n_species);
+
+  int return_flag = 0;
+
+  for( unsigned int i = 0; i < n_T_samples; i++ )
+    {
+      const Scalar T = T0 + T_inc*static_cast<Scalar>(i);
+      const Scalar rho = P/(R_mix*T);
+      chem_mixture.molar_densities(rho,Y,molar_densities);
+
+      typedef typename Antioch::CEAThermodynamics<Scalar>::template Cache<Scalar> Cache;
+
+      thermo.h_RT_minus_s_R(Cache(T),h_RT_minus_s_R);
+
+      kinetics.compute_mass_sources( T, rho, R_mix, Y, molar_densities, h_RT_minus_s_R, omega_dot );
+
+      // Omega dot had better sum to 0.0
+      Scalar sum = 0;
+      for( unsigned int s = 0; s < n_species; s++ )
+	{
+	  sum += omega_dot[s];
+	}
+      const Scalar sum_tol = std::numeric_limits<Scalar>::epsilon() * 1e6; // 1.0e-10;
+      if( std::fabs( sum ) > sum_tol )
+	{
+	  return_flag = 1;
+	  std::cerr << "Error: omega_dot did not sum to 0.0." << std::endl
+		    << std::scientific << std::setprecision(16)
+		    << "T = " << T << std::endl
+		    << "sum = " << sum << ", sum_tol = " << sum_tol << std::endl;
+	  for( unsigned int s = 0; s < n_species; s++ )
+	    {
+	      std::cerr << std::scientific << std::setprecision(16)
+			<< "omega_dot(" << chem_mixture.chemical_species()[s]->species() << ") = "
+			<< omega_dot[s] << std::endl;
+	    }
+	  std::cout << std::endl << std::endl;
+	}
+    }
+  
+  return return_flag;
+}
+
+
+template <typename Scalar>
 int tester(const std::string& input_name)
 {
   std::vector<std::string> species_str_list;
@@ -129,8 +203,16 @@ int main(int argc, char* argv[])
       antioch_error();
     }
 
-  return (tester<double>(std::string(argv[1])) ||
-          tester<long double>(std::string(argv[1])) ||
-          tester<float>(std::string(argv[1])));
+  int return_flag=0;
+
+  return_flag += (tester<double>(std::string(argv[1])) ||
+                  tester<long double>(std::string(argv[1])) ||
+                  tester<float>(std::string(argv[1])));
+
+  return_flag += (tester_N2N<double>(std::string(argv[1])) ||
+                  tester_N2N<long double>(std::string(argv[1])) ||
+                  tester_N2N<float>(std::string(argv[1])));
+
+  return return_flag;
 }
 
