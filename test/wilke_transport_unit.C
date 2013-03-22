42100b14482a360fd1f655af01163a1d637a59ca
Paul T. Bauman <ptbauman@gmail.com>
2013-03-22 21:54:42 +0000

    [Antioch]: Unit testing phi (#2764).

--- test/wilke_transport_unit.C
+++ test/wilke_transport_unit.C
@@ -28,6 +28,7 @@
 
 // C++
 #include <iostream>
+#include <cmath>
 
 // Antioch
 #include "antioch/chemical_mixture.h"
@@ -39,17 +40,17 @@
 #include "antioch/blottner_parsing.h"
 
 template <typename Scalar>
-int test_mu( const Scalar mu, const Scalar mu_exact, const Scalar tol )
+int test_val( const Scalar val, const Scalar val_exact, const Scalar tol, const std::string& val_name )
 {
   int return_flag = 0;
 
-  const Scalar rel_error = std::fabs( (mu - mu_exact)/mu_exact);
+  const Scalar rel_error = std::fabs( (val - val_exact)/val_exact);
 
   if( rel_error  > tol )
     {
-      std::cerr << "Error: Mismatch in thermal conductivity" << std::endl
-		<< "mu       = " << mu << std::endl
-		<< "mu_exact = " << mu_exact << std::endl
+      std::cerr << "Error: Mismatch in " << val_name << std::endl
+		<< val_name << "    = " << val << std::endl
+		<< val_name+"_exact = " << val_exact << std::endl
 		<< "rel_error = " << rel_error << std::endl
 		<< "tol = " << tol << std::endl;
       return_flag = 1;
@@ -92,13 +93,48 @@ int tester()
                            Antioch::EuckenThermalConductivity<Antioch::StatMechThermodynamics<Scalar> >,
                            Scalar > wilke( wilke_mixture, mu, k );
 
-  const Scalar T = 1000.0L;
-
-  std::vector<Scalar> mass_fractions( 5, 0.2L);
-
   int return_flag = 0;
 
   const Scalar tol = std::numeric_limits<Scalar>::epsilon() * 2;
+
+  // First check phi calculation
+  // phi_s = sum_r (chi_r*(1+sqrt(mu_s/mu_r)*(Mr/Ms)^(1/4))^2)/sqrt(8*(1+Ms/Mr))
+  {
+    std::vector<Scalar> mu( 5 );
+    mu[0] = 0.1L;
+    mu[1] = 0.2L;
+    mu[2] = 0.3L;
+    mu[3] = 0.15L;
+    mu[4] = 0.25L;
+
+    std::vector<Scalar> chi( 5 );
+    chi[0] = 0.1L;
+    chi[1] = 0.2L;
+    chi[2] = 0.3L;
+    chi[3] = 0.15L;
+    chi[4] = 0.25L;
+
+    Scalar phi_N_exact = 0.0L;
+    unsigned int N_index = 2;
+    Scalar M_N = chem_mixture.M(N_index);
+
+    for( unsigned int r = 0; r < 5; r++ )
+      {
+        Scalar M_r = chem_mixture.M(r);
+        Scalar dummy = 1.0L + std::sqrt(mu[N_index]/mu[r])*std::pow( M_r/M_N, 0.25L );
+        phi_N_exact += chi[r]*dummy*dummy/std::sqrt(8.0L*( 1.0L + M_N/M_r ) );
+      }
+
+    Scalar phi_N;
+    phi_N = wilke.compute_phi( mu, chi, N_index );
+
+    return_flag = test_val( phi_N, phi_N_exact, tol, std::string("phi") );
+  }
+  
+
+  const Scalar T = 1000.0L;
+
+  std::vector<Scalar> mass_fractions( 5, 0.2L); 
 
   // Currently dummy
   const Scalar mu_exact = 1.0L;
@@ -114,7 +150,8 @@ int tester()
 
 int main()
 {
-  return ( tester<double>() ||
+  return ( tester<double>()  ||
            tester<long double>() ||
-           tester<float>() );
+           tester<float>()
+           );
 }
