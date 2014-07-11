06089752ccb7a6476cc58d682e247790061353ec
Sylvain Plessis <sylvain.plessis@gmail.com>
2014-07-10 23:55:55 -0500

    xml, read, and enum updates         - efficiencies are no more necessarily double         - TROE_FALLOFF_LOW_NAME chemkin is compiling, need debug

--- src/parsing/include/antioch/read_reaction_set_data.h
+++ src/parsing/include/antioch/read_reaction_set_data.h
@@ -40,6 +40,7 @@
 #include "antioch/physical_constants.h"
 #include "antioch/units.h"
 #include "antioch/xml_parser.h"
+#include "antioch/chemkin_parser.h"
 
 // C++
 #include <string>
@@ -49,9 +50,9 @@
 namespace Antioch
 {
 
- /*!\file read_reaction_set_data_xml.h
+ /*!\file read_reaction_set_data.h
   *
-  * We parse the XML file here, with an exhaustive
+  * We parse the file here, with an exhaustive
   * unit management. The starting point is the kinetics
   * equation:
   * \f[
@@ -113,6 +114,11 @@ namespace Antioch
                                    const bool verbose,
                                    ReactionSet<NumericType>& reaction_set );
 
+  template<class NumericType>
+  void read_reaction_set_data_chemkin( const std::string& filename,
+                                       const bool verbose,
+                                       ReactionSet<NumericType>& reaction_set );
+
 
   template<typename NumericType, typename ParserType>
   void read_reaction_set_data(const std::string &filename,
@@ -133,6 +139,15 @@ namespace Antioch
                                    ReactionSet<NumericType>& reaction_set )
   {
      read_reaction_set_data<NumericType,XMLParser<NumericType> >(filename,verbose,reaction_set);
+  }
+
+  template<class NumericType>
+  inline
+  void read_reaction_set_data_chemkin( const std::string& filename,
+                                       const bool verbose,
+                                       ReactionSet<NumericType>& reaction_set )
+  {
+     read_reaction_set_data<NumericType,ChemKinParser<NumericType> >(filename,verbose,reaction_set);
   }
 
   template <typename NumericType>
@@ -661,7 +676,7 @@ namespace Antioch
         }
 
 
-        std::vector<std::pair<std::string,double> > efficiencies;
+        std::vector<std::pair<std::string,NumericType> > efficiencies;
         //efficiencies are only for three body reactions
         if(parser.efficiencies(efficiencies))
           {
