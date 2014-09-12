5ffc0f978651da32ae202c84c0943300fdbfe993
Sylvain Plessis <sylvain.plessis@gmail.com>
2014-09-12 14:53:42 -0500

    new files added

---
+++ src/core/include/antioch/transport_mixture.h
@@ -0,0 +1,181 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// Antioch - A Gas Dynamics Thermochemistry Library
+//
+// Copyright (C) 2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+//
+// $Id$
+//
+//--------------------------------------------------------------------------
+//--------------------------------------------------------------------------
+
+#ifndef ANTIOCH_TRANSPORT_MIXTURE_H
+#define ANTIOCH_TRANSPORT_MIXTURE_H
+
+// Antioch
+#include "antioch/antioch_asserts.h"
+#include "antioch/transport_species.h"
+#include "antioch/metaprogramming.h"
+#include "antioch/chemical_mixture.h"
+#include "antioch/transport_species_ascii_parsing.h"
+
+// C++
+#include <vector>
+#include <string>
+
+namespace Antioch
+{
+  //! Class storing chemical mixture properties
+  /*!
+    This class manages the list of TransportSpecies for a requested set
+    of species from input.
+  */
+  template<typename ThermoEvaluator,typename CoeffType=double>
+  class TransportMixture
+  {
+  public:
+    
+    TransportMixture( const ChemicalMixture<CoeffType> &mixture, const ThermoEvaluator & t,
+                      const std::string & filename = (std::string(ANTIOCH_DEFAULT_FILES_PATH) + std::string(ANTIOCH_DEFAULT_TRANSPORT_DATA)));
+
+    ~TransportMixture();
+
+    //! ChemicalMixture method
+    unsigned int n_species() const;
+
+    //! ChemicalMixture method
+    const std::vector<Species>& species_list() const;
+
+    //! \returns the chemical mixture
+    const ChemicalMixture<CoeffType> & chemical_mixture() const;
+
+    void add_species( const unsigned int index,
+                      CoeffType LJ_depth, CoeffType LJ_diameter,
+                      CoeffType dipole_moment, CoeffType polarizability, CoeffType rotational_relaxation, CoeffType mass);
+
+    const std::vector<TransportSpecies<CoeffType>*>& transport_species() const;
+
+  protected:
+
+    const ChemicalMixture<CoeffType>        & _chemical_mixture;
+
+    const ThermoEvaluator & _thermo;
+
+    std::vector<TransportSpecies<CoeffType>*> _transport_species;
+
+  private:
+    TransportMixture();
+
+  };
+
+
+  /* ------------------------- Inline Functions -------------------------*/
+
+  template<typename CoeffType>
+  inline
+  unsigned int TransportMixture<CoeffType>::n_species() const
+  {
+    return _chemical_mixture.n_species();
+  }
+
+  template<typename CoeffType>
+  inline
+  const std::vector<Species>& TransportMixture<CoeffType>::species_list() const
+  { 
+    return _chemical_mixture.species_list();
+  }
+
+  template<typename CoeffType>
+  inline
+  const ChemicalMixture<CoeffType> & TransportMixture<CoeffType>::chemical_mixture() const
+  {
+     return _chemical_mixture;
+  }
+
+  template<typename CoeffType>
+  inline
+  const std::vector<TransportSpecies<CoeffType>*>& TransportMixture<CoeffType>::transport_species() const
+  {
+    return _transport_species;
+  }
+
+
+  template<typename CoeffType>
+  inline
+  TransportMixture<CoeffType>::TransportMixture( const ChemicalMixture<CoeffType>& chem_mix, const ThermoEvaluator & t, const std::string & filename )
+    : _chemical_mixture( chem_mix),
+      _thermo(t),
+      _transport_species(_chemical_mixture.n_species(), NULL )
+  {
+
+    // Now read in transport properties for the requested species and stash
+    read_transport_species_data_ascii(*this, filename);
+
+    // check we have everyone requested
+    for( unsigned int s = 0; s < _transport_species.size(); ++s )
+      {
+        if(!_transport_species[s]) // it is not mandatory, Blottner or Sutherland are self-sufficient
+        {
+           std::cerr << "Warning: missing transport data for species " << _chemical_mixture.species_inverse_name_map().at(
+                                                                          _chemical_mixture.species_list()[s]) << "\n"
+                     << "Be sure to use a transport model that does not require the default data"
+                     << std::endl;
+        }
+      }
+
+    return;
+
+  }
+
+
+  template<typename CoeffType>
+  inline
+  TransportMixture<CoeffType>::~TransportMixture()
+  {
+    // Clean up all the TransportSpecies we stored
+    for( typename std::vector<TransportSpecies<CoeffType>* >::iterator it = _transport_species.begin();
+         it < _transport_species.end(); ++it )
+      {
+        delete (*it);
+      }
+
+    return;
+  }
+
+  template<typename CoeffType>
+  inline
+  void TransportMixture<CoeffType>::add_species( const unsigned int index,
+                                                CoeffType LJ_depth,
+                                                CoeffType LJ_diameter,
+                                                CoeffType dipole_moment,
+                                                CoeffType polarizability, 
+                                                CoeffType rotational_relaxation,
+                                                CoeffType mass)
+  {
+    Species name_enum = _chemical_mixture.species_list()[index];
+    _transport_species[index] =
+      new TransportSpecies<CoeffType>(name_enum, LJ_depth, LJ_diameter, dipole_moment, polarizability, rotational_relaxation,mass);
+
+    return;
+  }
+
+} // end namespace Antioch
+
+#endif //ANTIOCH_TRANSPORT_MIXTURE_H
