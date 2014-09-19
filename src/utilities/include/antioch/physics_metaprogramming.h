17f790d54487be60d822183a8dcb493402dba9c9
Sylvain Plessis <sylvain.plessis@gmail.com>
2014-09-19 02:15:10 -0500

    metaprogramming scheme

--- src/utilities/include/antioch/physics_metaprogramming.h
+++ src/utilities/include/antioch/physics_metaprogramming.h
@@ -37,38 +37,42 @@ namespace Antioch
    void physical_set_initialize(ModelSet & mod, default_physical_tag ){mod.set() = NULL;}
 
    template <typename ModelSet>
-   void physical_set_initialize(ModelSet & mod, physical_set_type_tag ){mod.set().resize(mod.mixture().n_species(),NULL);}
+   void physical_set_initialize(ModelSet & mod, default_physical_set_tag ){mod.set().resize(mod.mixture().n_species(),NULL);}
 
    // delete
-   template <typename Model>
-   void physical_set_delete(Model & set, default_physical_tag) {delete set;}
+   template <typename ModelSet>
+   void physical_set_delete(ModelSet & mod, default_physical_tag) {delete mod.set();}
 
-   template <typename Model>
-   void physical_set_delete(Model & set, physical_set_tag)
+   template <typename ModelSet>
+   void physical_set_delete(ModelSet & mod, default_physical_set_tag)
    {
-      for(set::iterator it = set.begin(); it != set.end(); ++it)
+      for(unsigned int s = 0; s < mod.mixture().n_species(); ++s)
       {
-         delete (*it);
+         delete mod.set()[s];
       }
    }
 
    // add general model
    template <typename Model, typename InitType>
-   void physical_set_add(SetOrEquation<Model,is_physical_set<Model>::value>::type  & set, const InitType & init, default_physical_tag)
+   void physical_set_add(typename SetOrEquation<Model,is_physical_set<Model>::value>::type & set, const InitType & init, default_physical_tag)
    {
+      antioch_assert(!set);
       set = new Model(init);
    }
 
    template <typename Model, typename InitType>
-   void physical_set_add(SetOrEquation<Model,is_physical_set<Model>::value>::type  & set, const InitType & init, physical_set_tag){}
+   void physical_set_add(typename SetOrEquation<Model,is_physical_set<Model>::value>::type & set, const InitType & init, default_physical_set_tag){}
 
    // add species model
    template <typename Model, typename InitType>
-   void physical_set_add(unsigned int s, SetOrEquation<Model,is_physical_set<Model>::value>::type & set, const InitType & init, default_physical_tag){}
+   void physical_set_add(unsigned int s, typename SetOrEquation<Model,is_physical_set<Model>::value>::type & set, const InitType & init, default_physical_tag){}
 
    template <typename Model, typename InitType>
-   void physical_set_add(unsigned int s, SetOrEquation<Model,is_physical_set<Model>::value>::type & set, const InitType & init, physical_set_tag)
+   void physical_set_add(unsigned int s, typename SetOrEquation<Model,is_physical_set<Model>::value>::type & set, const InitType & init, default_physical_set_tag)
    {
+      antioch_assert_less(s,set.size());
+      antioch_assert(!set[s]);
+
       set[s] = new Model(init);
    }
 
@@ -80,46 +84,97 @@ namespace Antioch
    }
 
    template <typename Model, typename InitType>
-   void physical_set_reset(Model & set, const InitType & init, physical_set_tag){}
+   void physical_set_reset(Model & set, const InitType & init, default_physical_set_tag){}
 
    // reset species model
    template <typename Model, typename InitType>
    void physical_set_reset(unsigned int s, Model & set, const InitType & init, default_physical_tag){}
 
    template <typename Model, typename InitType>
-   void physical_set_reset(unsigned int s, Model & set, const InitType & init, physical_set_tag)
+   void physical_set_reset(unsigned int s, Model & set, const InitType & init, default_physical_set_tag)
    {
       set[s]->reset_coeffs(init);
    }
 
-   template<typename Model, typename StateType>
-   ANTIOCH_AUTO(StateType)
-        physical_set_operator_viscosity(const Model & set, unsigned int s, const StateType & T, default_physical_tag)
+   // print
+   template <typename Model>
+   void physical_set_print(const Model & set, const std::map<unsigned int, std::string> & spec, std::ostream & out, default_physical_tag)
    {
-        antioch_error();// something's undefined
-        return -1;
+      out << "Model used for species:" << std::endl;
+      for(std::map<unsigned int, std::string>::const_iterator it = spec.begin(); it != spec.end(); ++it)
+      {
+          out << it->second << " "; 
+      }
+      out << std::endl << (*set) << std::endl;
    }
+
+   template <typename Model>
+   void physical_set_print(const Model & set, const std::map<unsigned int, std::string> & spec, std::ostream & out, default_physical_set_tag)
+   {
+      out << "Model per species: " << std::endl;
+
+      for(unsigned int s = 0; s < spec.size(); ++s)
+      {
+          out << "\t" << spec.at(s) << ": " << *set[s] << std::endl;
+      }
+   }
+
+
+/// operators
+
+   /// viscosity
+   // keep it this way of void it?
+   template<typename Model, typename StateType>
+   void physical_set_operator_viscosity(const Model & set, unsigned int s, const StateType & T, StateType & mu, default_physical_tag)
+   {}
+
+   // supposed to disappear and be optimized out if not needed
+   template<typename Model, typename StateType, typename VectorStateType>
+   void physical_set_operator_viscosity(const Model & set, const StateType & T, VectorStateType & mu, default_physical_tag)
+   {}
+
+   /// thermal conduction
+   // keep it this way of void it?
+   template<typename Model, typename StateType>
+   void physical_set_operator_thermal_conductivity(const Model & set, unsigned int s, const StateType & mu, const StateType & dss, const StateType & T, const StateType & rho, 
+                                                    StateType & k, default_physical_tag)
+   {}
+
+   // supposed to disappear and be optimized out if not needed
+   template<typename Model, typename StateType, typename VectorStateType>
+   void physical_set_operator_thermal_conductivity(const Model & set, const VectorStateType & mu, 
+                                                   const VectorStateType & dss, const StateType & T, const StateType & rho, 
+                                                    default_physical_tag)
+   {}
+
+
+   /// diffusion
+
+   // supposed to disappear and be optimized out
+   template<typename Model, typename StateType, typename MatrixStateType>
+   void physical_set_operator_diffusion(const Model & set, const StateType & T, const StateType & cTot,
+                                        MatrixStateType & Ds, default_physical_tag)
+   {}
+
+   // suppose to disappear and be optimized out
+   template<typename Model, typename StateType>
+   void physical_set_operator_diffusion(unsigned int s, const Model & set, const StateType & T, const StateType & cTot,
+                                         StateType & dss, default_physical_tag)
+   {}
+
+   // suppose to disappear and be optimized out
+   template<typename Model, typename StateType>
+   void physical_set_operator_diffusion(const Model & set, const StateType & cp, const StateType & k, StateType & ds, default_physical_tag)
+   {}
 
    // suppose to disappear and be optimized out
    template<typename Model, typename StateType, typename VectorStateType>
-   void physical_set_operator_thermal_conductivity(const Model & set, unsigned int s, const StateType & mu, const StateType & dss, const StateType & T, const StateType & rho, 
-                                                   StateType & k, default_physical_tag)
+   void physical_set_operator_diffusion(const Model & set, const VectorStateType & cp, const VectorStateType & k, VectorStateType & ds, default_physical_tag)
    {}
 
    // suppose to disappear and be optimized out
-   template<typename Model, typename StateType, typename MatrixStateType>
-   void physical_set_operator_diffusion_comes_first(const Model & set, const StateType & T, const StateType & cTot,
-                                     MatrixStateType & Ds, default_physical_tag)
-   {}
-
-   // suppose to disappear and be optimized out
-   template<typename Model, typename StateType>
-   void physical_set_operator_diffusion_comes_last(const Model & set, const StateType & cp, const StateType & k, StateType & ds, default_physical_tag)
-   {}
-
-   // suppose to disappear and be optimized out
-   template <typename MatrixStateType, typename VectorStateType>
-   void wilke_diffusion_rule(const MatrixStateType & Ds, VectorStateType & ds,default_physical_tag)
+   template <typename Mixture, typename MatrixStateType, typename VectorStateType>
+   void wilke_diffusion_rule(const Mixture & mixture, const VectorStateType & mass_fractions, const MatrixStateType & Ds, VectorStateType & ds, default_physical_tag)
    {}
 
 }
