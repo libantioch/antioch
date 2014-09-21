e2975e249ee56e58a9b3035288297b934de628ef
Sylvain Plessis <sylvain.plessis@gmail.com>
2014-09-20 22:46:07 -0500

    tag + licence

--- src/viscosity/include/antioch/pure_species_viscosity_utils.h
+++ src/viscosity/include/antioch/pure_species_viscosity_utils.h
@@ -1,5 +1,26 @@
 //-----------------------------------------------------------------------bl-
 //--------------------------------------------------------------------------
+//
+// Antioch - A Gas Dynamics Thermochemistry Library
+//
+// Copyright (C) 2014 Paul T. Bauman, Benjamin S. Kirk, Sylvain Plessis,
+//                    Roy H. Stonger
+// Copyright (C) 2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
 //-----------------------------------------------------------------------el-
 
 #ifndef ANTIOCH_PURE_SPECIES_VISCOSITY_UTILS_H
@@ -60,7 +81,7 @@ namespace Antioch
    }
 
    template<typename Model, typename StateType, typename VectorStateType>
-   void physical_set_operator_viscosity(const Model & set, const StateType & T, VectorStateType & mu, sutherland_viscosity_tag)
+   void physical_set_operator_viscosity(const Model & set, const StateType & T, VectorStateType & mu, pure_species_viscosity_tag)
    {
       antioch_assert_equal_to(mu.size(), set.size());
 
