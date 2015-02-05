9d71d82fe9b8aa0beae562dd7ca333930c46ceb0
Sylvain Plessis <sylvain.plessis@gmail.com>
2015-02-05 16:08:38 -0600

    Constant Lewis model metaprogramming specialization         - declaration after object definition         - implementation in separate file

--- src/diffusion/include/antioch/constant_lewis_diffusivity_utils_decl.h
+++ src/diffusion/include/antioch/constant_lewis_diffusivity_utils_decl.h
@@ -22,42 +22,26 @@
 // Boston, MA  02110-1301  USA
 //
 //-----------------------------------------------------------------------el-
-//
-// $Id$
-//
-//--------------------------------------------------------------------------
-//--------------------------------------------------------------------------
 
-#ifndef ANTIOCH_CONSTANT_LEWIS_DIFFUSIVITY_H
-#define ANTIOCH_CONSTANT_LEWIS_DIFFUSIVITY_H
+#ifndef ANTIOCH_CONSTANT_LEWIS_DIFFUSIVITY_UTILS_DECL_H
+#define ANTIOCH_CONSTANT_LEWIS_DIFFUSIVITY_UTILS_DECL_H
 
-#include "antioch/metaprogramming_decl.h" // ANTIOCH_AUTO*
+#include "antioch/physics_metaprogramming_decl.h"
 
 namespace Antioch
 {
+   // tag
+   struct constant_lewis_diffusivity_tag{};
 
-  template<typename CoeffType=double>
-  class ConstantLewisDiffusivity
-  {
-  protected:
+   // getting tag
+   template <typename CoeffType>
+   struct physical_tag<ConstantLewisDiffusivity<CoeffType> >;
 
-    CoeffType _Le;
-    
-  public:
-    
-    ConstantLewisDiffusivity( const CoeffType Le ) : _Le(Le) {}
+   template<typename Model, typename StateType>
+   void physical_set_operator_diffusion(const Model & set, const StateType & rho, const StateType & cp, const StateType & k, StateType & ds, constant_lewis_diffusivity_tag);
 
-    ~ConstantLewisDiffusivity() {}
-    
-    template<typename StateType>
-    ANTIOCH_AUTO(StateType)
-    D( const StateType& rho, const StateType& cp, const StateType& k ) const
-    ANTIOCH_AUTOFUNC(StateType, _Le*k/(rho*cp))
+   template<typename Model, typename StateType, typename VectorStateType>
+   void physical_set_operator_diffusion(const Model & set, const StateType & rho, const VectorStateType & cp, const VectorStateType & k, VectorStateType & ds, constant_lewis_diffusivity_tag);
+}
 
-  };
-
-} // end namespace Antioch
-
-#include "antioch/constant_lewis_diffusivity_utils_decl.h"
-
-#endif // ANTIOCH_CONSTANT_LEWIS_DIFFUSIVITY_H
+#endif
