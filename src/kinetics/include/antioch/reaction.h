19c93d132fe14d43ba29dd2ecb3f44a0f5b3445e
Roy H. Stogner <roystgnr@ices.utexas.edu>
2015-02-06 14:07:23 -0600

    Delete obsoleted, commented-out code

--- src/kinetics/include/antioch/reaction.h
+++ src/kinetics/include/antioch/reaction.h
@@ -324,9 +324,6 @@ namespace Antioch
     std::vector<unsigned int> _reactant_stoichiometry;
     std::vector<unsigned int> _product_stoichiometry;
     std::vector<CoeffType> _efficiencies;
-//    std::vector<unsigned int> _species_reactant_stoichiometry;
-//    std::vector<unsigned int> _species_product_stoichiometry;
-//    std::vector<int>          _species_delta_stoichiometry;
     int _gamma;
     bool _initialized;
     bool _reversible;
@@ -636,49 +633,17 @@ namespace Antioch
   inline
   void Reaction<CoeffType,VectorCoeffType>::initialize(unsigned int index)
   {
-    // Stoichiometric coefficients, by species id
-    {
-//      _species_reactant_stoichiometry.resize(this->n_species());
-//      _species_product_stoichiometry.resize(this->n_species());
-//      _species_delta_stoichiometry.resize(this->n_species());
-
-/*      std::fill( _species_reactant_stoichiometry.begin(),
-                 _species_reactant_stoichiometry.end(),
-                 0 );
-
-      std::fill( _species_product_stoichiometry.begin(),
-                 _species_product_stoichiometry.end(),
-                 0);
- */       }
-
     _gamma = 0;
     for (unsigned int r=0; r< this->n_reactants(); r++)
       {
-        /*_species_reactant_stoichiometry[this->reactant_id(r)] =
-          this->reactant_stoichiometric_coefficient(r);*/
         _gamma -= this->reactant_stoichiometric_coefficient(r);
       }
     
     for (unsigned int p=0; p < this->n_products(); p++)
       {
-        /*_species_product_stoichiometry[this->product_id(p)] =
-          this->product_stoichiometric_coefficient(p);*/
         _gamma += this->product_stoichiometric_coefficient(p);
       }
     
-    // find the delta stoichiometric coefficient for each species,
-    // and the sum of the deltas 
-    // Sylvain: useless
-/*    for (unsigned int s=0; s<this->n_species(); s++)
-      {
-        _species_delta_stoichiometry[s] =
-          ( _species_product_stoichiometry[s] -
-            _species_reactant_stoichiometry[s] );
-        
-       // _gamma += _species_delta_stoichiometry[s];
-      }
-*/ 
-
      // gives kinetics object index in reaction set
      for(typename std::vector<KineticsType<CoeffType,VectorCoeffType>* >::iterator it = _forward_rate.begin();
                 it != _forward_rate.end(); it++)
@@ -702,7 +667,6 @@ namespace Antioch
     // antioch_assert_greater( P0_RT, 0.0 );
     antioch_assert_greater( h_RT_minus_s_R.size(), 0 );
     antioch_assert_equal_to( h_RT_minus_s_R.size(), this->n_species() );
- //   antioch_assert_equal_to( _species_delta_stoichiometry.size(), this->n_species() );
 
 
 // DrG0 = - reactants + product
@@ -745,7 +709,6 @@ namespace Antioch
     antioch_assert_greater( h_RT_minus_s_R.size(), 0 );
     antioch_assert_equal_to( h_RT_minus_s_R.size(), this->n_species() );
     antioch_assert_equal_to( ddT_h_RT_minus_s_R.size(), this->n_species() );
- //   antioch_assert_equal_to( _species_delta_stoichiometry.size(), this->n_species() );
 
     // get the equilibrium constant
     keq = this->equilibrium_constant( P0_RT, h_RT_minus_s_R );
