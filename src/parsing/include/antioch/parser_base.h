ca90cdb5ea1cdfef3ab9d12d94b12821b61b48f1
Sylvain Plessis <sylvain.plessis@gmail.com>
2015-02-19 15:47:27 -0600

    forgotten file

--- src/parsing/include/antioch/parser_base.h
+++ src/parsing/include/antioch/parser_base.h
@@ -6,6 +6,7 @@
 #define ANTIOCH_PARSER_BASE_H
 
 //Antioch
+#include "antioch/parsing_enum.h"
 
 //C++
 #include <vector>
@@ -13,6 +14,11 @@
 
 namespace Antioch
 {
+
+  // Forward declarations
+  template <class NumericType>
+  class ChemicalMixture;
+
   /*!\class ParserBase
 
       A parser is an instance related to a file. The parser
@@ -43,105 +49,121 @@ namespace Antioch
   {
      public:
         ParserBase(const std::string & type, const std::string & file, bool verbose = true);
-        virtual ~ParserBase(){}
+        virtual ~ParserBase();
+
+        // to reinitialize, mandatory
+        virtual void change_file(const std::string & filename) = 0;
 
 /// species
         //! reads the species set
-        const std::vector<std::string> species_list() const {not_implemented(); return std::vector<std::string>();}
+        virtual const std::vector<std::string> species_list() const {not_implemented(); return std::vector<std::string>();}
 
         //! reads the mandatory data, not valid in xml
-        void read_chemical_species(ChemicalMixture<NumericType> & /*chem_mixture*/)  {not_implemented();}
+        virtual void read_chemical_species(ChemicalMixture<NumericType> & /*chem_mixture*/)  {not_implemented();}
 
         //! reads the vibrational data, not valid in xml
-        void read_vibrational_data(ChemicalMixture<NumericType> & /*chem_mixture*/)  {not_implemented();}
+        virtual void read_vibrational_data(ChemicalMixture<NumericType> & /*chem_mixture*/)  {not_implemented();}
 
         //! reads the electronic data, not valid in xml
-        void read_electronic_data(ChemicalMixture<NumericType> & /*chem_mixture*/)  {not_implemented();}
+        virtual void read_electronic_data(ChemicalMixture<NumericType> & /*chem_mixture*/)  {not_implemented();}
 
 /// reaction
 
          /*! go to next reaction*/
-         bool reaction() {not_implemented(); return false;}
+         virtual bool reaction() {not_implemented(); return false;}
 
          /*! go to next rate constant*/
-         bool rate_constant(const std::string & /*kinetics_model*/) {not_implemented(); return false;}
+         virtual bool rate_constant(const std::string & /*kinetics_model*/) {not_implemented(); return false;}
 
-         /*! return true if there's a Troe block*/
-         bool Troe()  {not_implemented(); return false;}
+         /*! \return true if there's a Troe block*/
+         virtual bool Troe()  {not_implemented(); return false;}
 
-         /*! return reaction id, 0 if not provided*/
-         const std::string reaction_id() const  {not_implemented(); return std::string();}
+         /*! \return reaction id, 0 if not provided*/
+         virtual const std::string reaction_id() const  {not_implemented(); return std::string();}
 
-         /*! return reaction equation */
-         const std::string reaction_equation() const {not_implemented(); return std::string();}
+         /*! \return reaction equation */
+         virtual const std::string reaction_equation() const {not_implemented(); return std::string();}
 
-         /*! return reaction chemical process*/
-         const std::string reaction_chemical_process() const {not_implemented(); return std::string();}
+         /*! \return reaction chemical process*/
+         virtual const std::string reaction_chemical_process() const {not_implemented(); return std::string();}
 
-         /*! return reaction kinetics model*/
-         const std::string reaction_kinetics_model(const std::vector<std::string> & /*kinetics_models*/) const  {not_implemented(); return std::string();}
+         /*! \return reaction kinetics model*/
+         virtual const std::string reaction_kinetics_model(const std::vector<std::string> & /*kinetics_models*/) const  {not_implemented(); return std::string();}
 
-         /*! return reversible state*/
-         bool reaction_reversible() const {not_implemented(); return false;}
+         /*! \return reversible state*/
+         virtual bool reaction_reversible() const {not_implemented(); return false;}
 
-         /*! return pairs of reactants and stoichiometric coefficients*/
-         bool reactants_pairs(std::vector<std::pair<std::string,int> > & /*reactants_pair*/) const  {not_implemented(); return false;}
+         /*! \return pairs of reactants and stoichiometric coefficients*/
+         virtual bool reactants_pairs(std::vector<std::pair<std::string,int> > & /*reactants_pair*/) const  {not_implemented(); return false;}
 
-         /*! return pairs of products and stoichiometric coefficients*/
-         bool products_pairs(std::vector<std::pair<std::string,int> > & /*products_pair*/) const {not_implemented(); return false;}
+         /*! \return pairs of products and stoichiometric coefficients*/
+         virtual bool products_pairs(std::vector<std::pair<std::string,int> > & /*products_pair*/) const {not_implemented(); return false;}
 
-         /*! return true if "name" attribute is found with value "k0"*/
-         bool is_k0(unsigned int nrc, const std::string & /*kin_model*/) const {not_implemented(); return false;}
+         /*! \return true if "name" attribute is found with value "k0"*/
+         virtual bool is_k0(unsigned int nrc, const std::string & /*kin_model*/) const {not_implemented(); return false;}
 
-         /*! return index of k0 (0 or 1)*/
-         unsigned int where_is_k0(const std::string & /*kin_model*/) const {not_implemented(); return -1;}
+         /*! \return index of k0 (0 or 1)*/
+         virtual unsigned int where_is_k0(const std::string & /*kin_model*/) const {not_implemented(); return -1;}
 
-         /*! return true if pre exponentiel coefficient*/
-         bool rate_constant_preexponential_parameter(NumericType & /*A*/, std::string & /*A_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
+         /*! \return true if pre exponentiel coefficient*/
+         virtual bool rate_constant_preexponential_parameter(NumericType & /*A*/, std::string & /*A_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
 
-         /*! return true if beta coefficient*/
-         bool rate_constant_power_parameter(NumericType & /*b*/, std::string & /*b_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
+         /*! \return true if beta coefficient*/
+         virtual bool rate_constant_power_parameter(NumericType & /*b*/, std::string & /*b_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
 
-         /*! return true if activation energie*/
-         bool rate_constant_activation_energy_parameter(NumericType & /*Ea*/, std::string & /*Ea_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
+         /*! \return true if activation energie*/
+         virtual bool rate_constant_activation_energy_parameter(NumericType & /*Ea*/, std::string & /*Ea_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
 
-         /*! return true if D coefficient*/
-         bool rate_constant_Berthelot_coefficient_parameter(NumericType & /*D*/, std::string & /*D_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
+         /*! \return true if D coefficient*/
+         virtual bool rate_constant_Berthelot_coefficient_parameter(NumericType & /*D*/, std::string & /*D_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
 
-         /*! return true if Tref*/
-         bool rate_constant_Tref_parameter( NumericType & /*Tref*/, std::string & /*Tref_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
+         /*! \return true if Tref*/
+         virtual bool rate_constant_Tref_parameter( NumericType & /*Tref*/, std::string & /*Tref_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
 
-         /*! return true if lambda*/
-         bool rate_constant_lambda_parameter(std::vector<NumericType> & /*lambda*/, std::string & /*lambda_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
+         /*! \return true if lambda*/
+         virtual bool rate_constant_lambda_parameter(std::vector<NumericType> & /*lambda*/, std::string & /*lambda_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
 
-         /*! return true if sigma*/
-         bool rate_constant_cross_section_parameter(std::vector<NumericType> & /*sigma*/,  std::string & /*sigma_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
+         /*! \return true if sigma*/
+         virtual bool rate_constant_cross_section_parameter(std::vector<NumericType> & /*sigma*/,  std::string & /*sigma_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
 
-         /*! return true if a Kooij is called Arrhenuis*/
-         bool verify_Kooij_in_place_of_Arrhenius() const {not_implemented(); return false;}
+         /*! \return true if a Kooij is called Arrhenuis*/
+         virtual bool verify_Kooij_in_place_of_Arrhenius() const {not_implemented(); return false;}
 
-         /*! return true if efficiencies are found*/
-         bool efficiencies(std::vector<std::pair<std::string,NumericType> > & /*par_values*/) const {not_implemented(); return false;}
+         /*! \return true if efficiencies are found*/
+         virtual bool efficiencies(std::vector<std::pair<std::string,NumericType> > & /*par_values*/) const {not_implemented(); return false;}
 
-         /*! return true is alpha*/
-         bool Troe_alpha_parameter(NumericType & /*alpha*/, std::string & /*alpha_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
+         /*! \return true is alpha*/
+         virtual bool Troe_alpha_parameter(NumericType & /*alpha*/, std::string & /*alpha_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
 
-         /*! return true is alpha*/
-         bool Troe_T1_parameter(NumericType & /*T1*/, std::string & /*T1_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
+         /*! \return true is alpha*/
+         virtual bool Troe_T1_parameter(NumericType & /*T1*/, std::string & /*T1_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
 
-         /*! return true is alpha*/
-         bool Troe_T2_parameter(NumericType & /*T2*/, std::string & /*T2_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
+         /*! \return true is alpha*/
+         virtual bool Troe_T2_parameter(NumericType & /*T2*/, std::string & /*T2_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
 
-         /*! return true is alpha*/
-         bool Troe_T3_parameter(NumericType & /*T3*/, std::string & /*T3_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
+         /*! \return true is alpha*/
+         virtual bool Troe_T3_parameter(NumericType & /*T3*/, std::string & /*T3_unit*/, std::string & /*def_unit*/) const {not_implemented(); return false;}
 
+         /*! \return name of file*/
+        const std::string file() const {return _file;}
+
+         /*! \return type of parser*/
+        const std::string type() const {return _type;}
+
+         /*! \return verbosity*/
+        const bool     verbose() const {return _verbose;}
+
+        /*! \return enum*/
+        ParsingType enum_type() const;
+        
 
      protected:
+
         std::string _type;
         std::string _file;
         bool        _verbose;
 
-        void not_implemented();
+        void not_implemented() const;
 
      private:
         ParserBase();
@@ -163,12 +185,37 @@ namespace Antioch
   }
 
   template <typename NumericType>
-  void ParserBase<NumericType>::not_implemented()
+  ParsingType ParserBase<NumericType>::enum_type() const
   {
-      std::cerr << "This method is not available with a " << _type << " parser.\n"
-                << "No format has been defined yet.  Maybe contribute?\n"
-                << "https://github.com/libantioch/antioch" << std::endl;
+      ParsingType PType(ASCII);
+      if(_type == "ascii")
+      {
+          PType = ASCII;
+      }else if(_type == "ChemKin")
+      {
+          PType = CHEMKIN;
+      }else if(_type == "XML")
+      {
+          PType = XML;
+      }else
+      {
+           antioch_parsing_error(std::string("unknown parser type!!! " + _type));
+      }
+      return PType;
   }
+
+  template <typename NumericType>
+  void ParserBase<NumericType>::not_implemented() const
+  {
+      std::cerr << "\n*********************************************************\n"
+                << "This method is not available with a " << _type << " parser.\n"
+                << "Parsing file " << _file << ".\n"
+                << "No format has been defined yet.  Maybe contribute?\n"
+                << "https://github.com/libantioch/antioch\n" 
+                << "\n*********************************************************\n"
+                << std::endl;
+  }
+
 
 } // end namespace Antioch
 
