f1f381dfa9835933d8e9bfbbb8409dd4734e197b
Paul T. Bauman <ptbauman@gmail.com>
2015-05-05 11:37:44 -0400

    Add MixtureTransportBase
    Holds common data/functionality to be used by MixtureViscosity
    and forthcoming MixtureConductivity and MixtureDiffusion

---
+++ src/transport/include/antioch/mixture_transport_base.h
@@ -0,0 +1,84 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// Antioch - A Gas Dynamics Thermochemistry Library
+//
+// Copyright (C) 2014 Paul T. Bauman, Benjamin S. Kirk, Sylvain Plessis,
+//                    Roy H. Stonger
+// Copyright (C) 2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#ifndef ANTIOCH_MIXTURE_TRANSPORT_BASE_H
+#define ANTIOCH_MIXTURE_TRANSPORT_BASE_H
+
+namespace Antioch
+{
+  template<typename ThermoEvaluator, class CoeffType=double>
+  class MixtureTransportBase
+  {
+  public:
+
+    MixtureTransportBase( const TransportMixture<ThermoEvaluator,CoeffType>& transport_mixture );
+
+    virtual ~MixtureTransportBase(){};
+
+    const ChemicalMixture<CoeffType>& chemical_mixture() const;
+
+    const TransportMixture<ThermoEvaluator,CoeffType>& transport_mixture() const;
+
+    const TransportMixture<ThermoEvaluator,CoeffType>& mixture() const;
+
+  protected:
+
+    const TransportMixture<ThermoEvaluator,CoeffType>& _transport_mixture;
+
+  private:
+
+    MixtureTransportBase();
+
+  };
+
+  template<typename ThermoEvaluator, class CoeffType>
+  MixtureTransportBase<ThermoEvaluator,CoeffType>::MixtureTransportBase( const TransportMixture<ThermoEvaluator,CoeffType>& transport_mixture )
+    : _transport_mixture(transport_mixture)
+  {}
+
+  template<typename ThermoEvaluator, class CoeffType>
+  inline
+  const ChemicalMixture<CoeffType>& MixtureTransportBase<ThermoEvaluator,CoeffType>::chemical_mixture() const
+  {
+    return _transport_mixture.chemical_mixture();
+  }
+
+  template<typename ThermoEvaluator, class CoeffType>
+  inline
+  const TransportMixture<ThermoEvaluator,CoeffType>& MixtureTransportBase<ThermoEvaluator,CoeffType>::transport_mixture() const
+  {
+    return _transport_mixture;
+  }
+
+  template<typename ThermoEvaluator, class CoeffType>
+  inline
+  const TransportMixture<ThermoEvaluator,CoeffType>& MixtureTransportBase<ThermoEvaluator,CoeffType>::mixture() const
+  {
+    return _transport_mixture;
+  }
+
+} // end namespace Antioch
+
+#endif // ANTIOCH_MIXTURE_TRANSPORT_BASE_H
