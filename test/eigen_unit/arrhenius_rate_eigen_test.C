55dc48636526423902dbc6926af3b5ee6aefd22e
Paul T. Bauman <ptbauman@gmail.com>
2016-02-05 12:38:23 -0500

    Update header for 2016

--- test/eigen_unit/arrhenius_rate_eigen_test.C
+++ test/eigen_unit/arrhenius_rate_eigen_test.C
@@ -3,8 +3,9 @@
 //
 // Antioch - A Gas Dynamics Thermochemistry Library
 //
-// Copyright (C) 2014 Paul T. Bauman, Benjamin S. Kirk, Sylvain Plessis,
-//                    Roy H. Stonger
+// Copyright (C) 2014-2016 Paul T. Bauman, Benjamin S. Kirk,
+//                         Sylvain Plessis, Roy H. Stonger
+//
 // Copyright (C) 2013 The PECOS Development Team
 //
 // This library is free software; you can redistribute it and/or
