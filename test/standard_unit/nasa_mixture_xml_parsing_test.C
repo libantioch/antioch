a57252d9917fd4be886f463baff2bcd71b434910
Paul T. Bauman <ptbauman@gmail.com>
2016-02-09 11:30:50 -0500

    Add CppUnit test for NASA9 XML parsing
    This doesn't yet test parsing the species-we-want-to-use from the XML
    file, but it does test fairly completely the parsed coefficients from
    the species present in the chemical mixture. We use the shared
    nasa9_thermo.xml as the test file so we're actually testing (for N2 and
    NO2 at least) the file that we're shipping.

---
+++ test/standard_unit/nasa_mixture_xml_parsing_test.C
@@ -0,0 +1,131 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// Antioch - A Gas Dynamics Thermochemistry Library
+//
+// Copyright (C) 2014-2016 Paul T. Bauman, Benjamin S. Kirk,
+//                         Sylvain Plessis, Roy H. Stonger
+//
+// Copyright (C) 2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+
+#include "antioch_config.h"
+
+#ifdef ANTIOCH_HAVE_CPPUNIT
+
+// C++
+#include <limits>
+
+// Antioch
+#include "nasa9_thermo_test_base.h"
+#include "antioch/chemical_mixture.h"
+#include "antioch/nasa9_curve_fit.h"
+#include "antioch/nasa_mixture.h"
+#include "antioch/nasa_mixture_parsing.h"
+
+namespace AntiochTesting
+{
+  template<typename Scalar>
+  class NASA9XMLParsingTest : public NASA9ThermoTestBase<Scalar>
+  {
+  public:
+
+    void test_supplied_species()
+    {
+      std::vector<std::string> species_str_list(2);
+      species_str_list[0] = "N2";
+      species_str_list[1] = "NO2";
+
+      Antioch::ChemicalMixture<Scalar> chem_mixture( species_str_list );
+      Antioch::NASAThermoMixture<Scalar, Antioch::NASA9CurveFit<Scalar> > nasa_mixture( chem_mixture );
+
+      std::string filename = std::string(ANTIOCH_SHARE_XML_INPUT_FILES_SOURCE_PATH)+"nasa9_thermo.xml";
+
+      Antioch::read_nasa_mixture_data( nasa_mixture, filename, Antioch::XML );
+
+      const Antioch::NASA9CurveFit<Scalar>& N2_curve_fit =  nasa_mixture.curve_fit(0);
+      const Antioch::NASA9CurveFit<Scalar>& NO2_curve_fit =  nasa_mixture.curve_fit(1);
+
+      CPPUNIT_ASSERT_EQUAL( (unsigned int)5, N2_curve_fit.n_intervals() );
+      CPPUNIT_ASSERT_EQUAL( (unsigned int)4, NO2_curve_fit.n_intervals() );
+
+      // Check N2 coefficients
+      this->check_coefficients( N2_curve_fit.coefficients(0), this->_N2_coeffs_0_200 );
+      this->check_coefficients( N2_curve_fit.coefficients(1), this->_N2_coeffs_200_1000 );
+      this->check_coefficients( N2_curve_fit.coefficients(2), this->_N2_coeffs_1000_6000 );
+      this->check_coefficients( N2_curve_fit.coefficients(3), this->_N2_coeffs_6000_20000 );
+      this->check_coefficients( N2_curve_fit.coefficients(4), this->_N2_coeffs_20000_99999 );
+
+      // Check NO2 coefficients
+      this->check_coefficients( NO2_curve_fit.coefficients(0), this->_NO2_coeffs_0_200 );
+      this->check_coefficients( NO2_curve_fit.coefficients(1), this->_NO2_coeffs_200_1000 );
+      this->check_coefficients( NO2_curve_fit.coefficients(2), this->_NO2_coeffs_1000_6000 );
+      this->check_coefficients( NO2_curve_fit.coefficients(3), this->_NO2_coeffs_6000_99999 );
+    }
+
+    void check_coefficients( const Scalar* parsed_coeffs, std::vector<Scalar>& exact_coeffs )
+    {
+      for( unsigned int c = 0; c < exact_coeffs.size(); c++ )
+        CPPUNIT_ASSERT_DOUBLES_EQUAL( exact_coeffs[c], parsed_coeffs[c], this->tol() );
+    }
+
+    Scalar tol()
+    { return std::numeric_limits<Scalar>::epsilon() * 10; }
+
+  };
+
+  class NASA9XMLParsingTestFloat : public NASA9XMLParsingTest<float>
+  {
+  public:
+    CPPUNIT_TEST_SUITE( NASA9XMLParsingTestFloat );
+
+    CPPUNIT_TEST(test_supplied_species);
+
+    CPPUNIT_TEST_SUITE_END();
+
+  };
+
+  class NASA9XMLParsingTestDouble : public NASA9XMLParsingTest<double>
+  {
+  public:
+    CPPUNIT_TEST_SUITE( NASA9XMLParsingTestDouble );
+
+    CPPUNIT_TEST(test_supplied_species);
+
+    CPPUNIT_TEST_SUITE_END();
+
+  };
+
+  class NASA9XMLParsingTestLongDouble : public NASA9XMLParsingTest<long double>
+  {
+  public:
+    CPPUNIT_TEST_SUITE( NASA9XMLParsingTestLongDouble );
+
+    CPPUNIT_TEST(test_supplied_species);
+
+    CPPUNIT_TEST_SUITE_END();
+
+  };
+
+  CPPUNIT_TEST_SUITE_REGISTRATION( NASA9XMLParsingTestFloat );
+  CPPUNIT_TEST_SUITE_REGISTRATION( NASA9XMLParsingTestDouble );
+  CPPUNIT_TEST_SUITE_REGISTRATION( NASA9XMLParsingTestLongDouble );
+
+} // end namespace AntiochTesting
+
+#endif // ANTIOCH_HAVE_CPPUNIT
