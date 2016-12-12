0178b62532301894465b3df8cff8e1518ba53799
Roy H. Stogner <roystgnr@ices.utexas.edu>
2016-12-12 15:41:14 -0600

    Bugfix for NASAEvaluator::dcp_dT
    I'll squash this commit later, once @dsondak is done testing and has
    hopefully caught any other stupid errors I've made.

--- src/thermo/include/antioch/nasa_evaluator.h
+++ src/thermo/include/antioch/nasa_evaluator.h
@@ -222,7 +222,7 @@ namespace Antioch
          Antioch::zero_clone (cache.T),
          StateType
            (this->chem_mixture().R(species) *
-            this->dcp_over_R_dT(cache, species)));
+            this->curve_fit(species).dcp_over_R_dT(cache)));
   }
 
   template<typename CoeffType, typename NASAFit>
